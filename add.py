572235abc24dc007b9c8224140ad943e453bf8d6
NANDINI MENON <13816@fisat>
2018-04-18 01:10:49 -0400

    Create function to add three numbers

--- add.py
+++ add.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python
-x=int(raw_input("Enter 1st number : "))
-y=int(raw_input("Enter 2nd number : "))
-print "Sum = ",x+y
+def add2():
+    x=int(raw_input("\nEnter 1st number : "))
+    y=int(raw_input("Enter 2nd number : "))
+    print "Sum = ",x+y
+
+def add3():
+    x=int(raw_input("\nEnter 1st number : "))
+    y=int(raw_input("Enter 2nd number : "))
+    z=int(raw_input("Enter 3rd number : "))
+    print "Sum = ",x+y+z
+
+print "MENU:-\n1. Add 2 numbers\n2. Add 3 numbers"
+ch=int(raw_input("Enter your choice : "))
+if (ch==1):
+    add2()
+elif (ch==2):
+    add3()
+else:
+    print "Invalid Choice"
